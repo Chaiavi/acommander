a34639d18581a9e509895e0402ea8d67dfeae95d
Chaiavi <avraham2@gmail.com>
2025-05-28 17:46:26 +0300

    Rewrite of the ActionImpl, now works better with paths and added copy

--- src/main/java/org/chaiware/acommander4j/BasicActionsImpl.java
+++ src/main/java/org/chaiware/acommander4j/BasicActionsImpl.java
@@ -1,37 +1,44 @@
 package org.chaiware.acommander4j;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BasicActionsImpl implements IActions {
-    @Override
-    public void edit(FileItem fileItem) throws IOException {
-        runExecutable("C:\\Users\\User\\IdeaProjects\\acommander4j\\apps\\TedNPad.exe", fileItem.getFile().toString());
-    }
+    private final String APP_PATH = Paths.get(System.getProperty("user.dir"), "apps") + "\\";
 
     @Override
     public void view(FileItem fileItem) throws IOException {
-        runExecutable("C:\\Users\\User\\Desktop\\tmp\\ql\\QuickLook.exe", fileItem.getFile().toString()); // QuickLook Best but 236mb
-        runExecutable("C:\\Users\\User\\Desktop\\tmp\\uv\\Viewer.exe", fileItem.getFile().toString()); // UniversalViewer least features but 10mb
-        runExecutable("C:\\Users\\User\\Desktop\\tmp\\fvl\\fv.exe", fileItem.getFile().toString()); // FileViewerLite 98mb quite good
+//        runExecutable("C:\\Users\\User\\Desktop\\tmp\\ql\\QuickLook.exe", fileItem.getFile().toString()); // QuickLook Best but 236mb
+//        runExecutable("C:\\Users\\User\\Desktop\\tmp\\uv\\Viewer.exe", fileItem.getFile().toString()); // UniversalViewer least features but 10mb
+//        runExecutable("C:\\Users\\User\\Desktop\\tmp\\fvl\\fv.exe", fileItem.getFile().toString()); // FileViewerLite 98mb quite good
+    }
+
+    @Override
+    public void edit(FileItem fileItem) throws IOException {
+        List<String> command = new ArrayList<>();
+        command.add(APP_PATH + "TedNPad.exe");
+        command.add(fileItem.getFile().toString());
+        runExecutable(command);
     }
 
     @Override
     public void copy(FileItem sourceFile, String targetFolder) throws IOException {
-        runExecutable("C:\\Users\\User\\Desktop\\tmp\\fc2\\FastCopy.exe", sourceFile.getFile().toString(), "/to=" + targetFolder); // QuickLook Best but 236mb
+        List<String> command = new ArrayList<>();
+        command.add(APP_PATH + "FastCopy.exe");
+        command.add("/cmd=diff");
+        command.add("/auto_close");
+        command.add("/force_close");
+        command.add(sourceFile.getFile().toString());
+        command.add("/to=" + targetFolder);
+        runExecutable(command);
+         // fastcopy.exe /cmd=diff /auto_close /force_close /no_confirm_stop "FastCopy.exe" /to=".\doc"
     }
 
-    private void runExecutable(String executable, String filename) throws IOException {
-        ProcessBuilder pb = new ProcessBuilder(executable, filename);
-        pb.directory(new File(filename).getParentFile());
-        pb.start();
-    }
-
-    private void runExecutable(String executable, String filename, String folderName) throws IOException {
-        ProcessBuilder pb = new ProcessBuilder(executable, filename, folderName);
-        pb.directory(new File(filename).getParentFile());
+    private void runExecutable(List<String> params) throws IOException {
+        ProcessBuilder pb = new ProcessBuilder(params);
+//        pb.directory(new File(filename).getParentFile());
         pb.start();
     }
 }
