ad83cb8f96a04a17f7741c1b3a075622d9e3e575
Chaiavi <avraham2@gmail.com>
2025-06-12 09:23:13 +0300

    Changed from interface to abstract added to the abstract some vars and common fuunctions

---
+++ src/main/java/org/chaiware/acommander/ACommands.java
@@ -0,0 +1,55 @@
+package org.chaiware.acommander;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class ACommands {
+    protected final String APP_PATH = Paths.get(System.getProperty("user.dir"), "apps") + "\\";
+    protected FilesPanesHelper fileListsLoader;
+    final Logger log = LoggerFactory.getLogger(ACommands.class);
+
+    abstract void rename(FileItem selectedItem, String newFilename) throws Exception;
+    abstract void edit(FileItem fileItem) throws Exception;
+    abstract void view(FileItem fileItem) throws Exception;
+    abstract void copy(FileItem sourceFile, String targetFolder) throws Exception;
+    abstract void move(FileItem sourceFile, String targetFolder) throws Exception;
+    abstract void mkdir(String parentDir, String newDirName) throws IOException;
+    abstract void delete(FileItem selectedItem) throws IOException;
+    abstract void openTerminal(String openHerePath) throws Exception;
+    abstract void searchFiles(String sourcePath, String filenameWildcard) throws Exception;
+    abstract void pack(FileItem selectedItem, String archiveFilename, String destinationPath) throws Exception;
+    abstract void unpack(FileItem selectedItem, String destinationPath) throws Exception;
+
+    public ACommands(FilesPanesHelper filesPanesHelper) {
+        this.fileListsLoader = filesPanesHelper;
+    }
+
+    protected List<String> runExecutable(List<String> params, boolean isWaitFor) throws IOException, InterruptedException {
+        ProcessBuilder pb = new ProcessBuilder(params);
+        pb.redirectErrorStream(true); // merges stderr into stdout
+//        pb.directory(new File("C:\\Users\\Hayun\\IdeaProjects\\acommander\\apps"));
+        log.debug("Running: {}", String.join(" ", pb.command()));
+        Process process = pb.start();
+        List<String> output = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.add(line);
+            }
+        }
+
+        if (isWaitFor) {
+            process.waitFor();
+            fileListsLoader.refreshFileListViews();
+        }
+
+        return output;
+    }
+}
