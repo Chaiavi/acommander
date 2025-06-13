70880ee74e6e80cfd365535da3165bee0c334b1f
Chaiavi <avraham2@gmail.com>
2025-06-13 17:43:27 +0300

    Simplest stopwatch helper

---
+++ src/main/java/org/chaiware/acommander/helpers/Stopwatch.java
@@ -0,0 +1,30 @@
+package org.chaiware.acommander.helpers;
+
+/** Simplest Stopwatch, it is created started, and toSting shows the current elapsed time */
+public class Stopwatch {
+    private final long startTime;
+
+    public Stopwatch() {
+        startTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public String toString() {
+        long ms = System.currentTimeMillis() - startTime;
+
+        if (ms < 1000) {
+            return ms + " ms";
+        } else if (ms < 60000) {
+            return String.format("%.2f s", ms / 1000.0);
+        } else if (ms < 3600000) {
+            long minutes = ms / 60000;
+            long seconds = (ms % 60000) / 1000;
+            return String.format("%d:%02d min", minutes, seconds);
+        } else {
+            long hours = ms / 3600000;
+            long minutes = (ms % 3600000) / 60000;
+            long seconds = (ms % 60000) / 1000;
+            return String.format("%d:%02d:%02d", hours, minutes, seconds);
+        }
+    }
+}
